da18c9ab7f491ccaa957ab4806f25a408d109e03
cte <christophepascale@yahoo.fr>
2020-09-29 11:57:30 +0200

    add admin and auth controller

---
+++ src/Controller/admin/post/edit.php
@@ -0,0 +1,61 @@
+<?php
+use App \Connection;
+use App \Table\PostTable;
+use App\Validator;
+use App\HTML\Form;
+use App\Validators\PostValidator;
+use App\ObjectHelper;
+use App\Auth;
+use App\Table\CategoryTable;
+
+
+Auth::check();
+
+$pdo = Connection:: getPDO();
+$postTable = new PostTable($pdo);
+$categoryTable = new CategoryTable($pdo);
+$categories = $categoryTable->list();
+$post = $postTable ->find($params['id']);
+$categoryTable->hydratePosts([$post]);
+$success = false;
+
+$errors = [];
+
+if (!empty($_POST)) {
+    $v = new PostValidator($_POST, $postTable, $post->getID(), $categories);
+    ObjectHelper::hydrate($post, $_POST, ['name', 'content', 'slug', 'created_at']);
+    if ($v->validate()) {
+        $pdo->beginTransaction();
+        $postTable->updatePost($post);
+        $postTable->attachCategories($post->getID(),$_POST['categories_ids']);
+        $pdo->commit();
+        $categoryTable->hydratePosts([$post]);
+        $success = true;
+    } else {
+       $errors = $v->errors();  
+    }
+}
+$form = new Form($post, $errors);
+?>
+
+<?php if ($success): ?>
+<div class="alert alert-success">
+L'article a ete correctement modifié!
+</div>
+<?php endif ?>
+
+<?php if (isset($_GET['created'])): ?>
+<div class="alert alert-success">
+L'article a ete correctement crée
+</div>
+<?php endif ?>
+
+<?php if (!empty($errors)): ?>
+<div class="alert alert-danger">
+L'article n'a pas pu être modifié
+</div>
+<?php endif ?>
+
+<h1>Editer l'article <?= e($post->getName()) ?></h1>
+
+<?php require('_form.php') ?>
