0fece7cd6dc6d1c535b918168e64748342cbbc07
cte <christophepascale@yahoo.fr>
2020-10-06 18:20:59 +0200

    page accueil formulaire footer insert theme

--- views/home/index.php
+++ views/home/index.php
@@ -1,81 +1,59 @@
 
-    <div class="jumbotron jumbotron-fluid">
-        <div class="container">
-            <div class="row">
-            <div class="col-12 text-center">
-            <h1>Un projet à développer?   -  Ne le faite pas seul </h1>
+  
+   <div class="container">
+    <div class="row">
+      <div class="col-lg-12 col-md-10 mx-auto text-center">
+        <h4> laissez-moi  message <br>   Je vous réponds dès que possible</h4>
+        <!-- Contact Form - Enter your email address on line 19 of the mail/contact_me.php file to make this form work. -->
+        <!-- WARNING: Some web hosts do not allow emails to be sent through forms to common mail hosts like Gmail or Yahoo. It's recommended that you use a private domain email address! -->
+        <!-- To use the contact form, your site must be on a live web host with PHP! The form will not work locally! -->
+        <div style="padding-left: 15px; margin-top:30px; border: 4px solid #A0A0A0; text-align:left;">
+        <form name="sentMessage" id="contactForm" novalidate>
+          <div class="control-group">
+            <div class="form-group floating-label-form-group controls">
+              <label>NOM</label>
+              <input type="text" class="form-control" placeholder="Nom" id="name" required data-validation-required-message="Please enter your name.">
+              <p class="help-block text-danger"></p>
             </div>
+          </div>
+          <div class="control-group">
+            <div class="form-group floating-label-form-group controls">
+              <label>Adresse Mail</label>
+              <input type="email" class="form-control" placeholder="Adresse Mail" id="email" required data-validation-required-message="Please enter your email address.">
+              <p class="help-block text-danger"></p>
             </div>
-        </div>
-        
-        <div class="row">
-            <div class="col lg-10 text-center">
-            <h4>Elève chez OpenClassrooms <br>  Développeur Application PHP Synfony</h4><br>
-            <h3><strong> Pascale CHRISTOPHE</strong></h3><br>
+          </div>
+          <div class="control-group">
+            <div class="form-group col-xs-12 floating-label-form-group controls">
+              <label>Télephone</label>
+              <input type="tel" class="form-control" placeholder="Numéro de Téléphone" id="phone" required data-validation-required-message="Please enter your phone number.">
+              <p class="help-block text-danger"></p>
             </div>
-            <div class="col lg-2 ">
-            <img src="/photo.png" alt="photo" class="rounded-circle">
+          </div>
+          <div class="control-group">
+            <div class="form-group floating-label-form-group controls">
+              <label>Votre message</label>
+              <textarea rows="5" class="form-control" placeholder="Message" id="message" required data-validation-required-message="Please enter a message."></textarea>
+              <p class="help-block text-danger"></p>
             </div>
-        </div>
-            
-        </div>
-        </div>
+          </div>
+</div>
+          <br>
+
+
+
+
+           
+          <div id="success"></div>
+          <button type="submit" class="btn btn-secondary" id="sendMessageButton">Envoyer</button>
+        </form>
+      </div>
     </div>
+  </div>
 
 <body>
-<div class="container">
- <form action="contact_formulaire.php" method="POST">
-     <div class= "row ">
-         <div class="col-6">
-                <div class="form-group text-center">
-                  <label for="inputname"><strong> Votre nom</strong></label>
-                  <input type ="text" name= "name" class="form-control" id="inputname">
-                </div>
-            </div>
-         <div class="col-6">
-                <div class="form-group text-center">
-                  <label for="inputname"><strong> Votre prénom</strong></label>
-                  <input type ="text" name= "surname" class="form-control" id="inputname">
-                </div>
-         </div>
-     </div>
 
 
-     <div class= "row ">
-         <div class="col-6">
-                <div class="form-group text-center">
-                  <label for="inputname"><strong>Votre Email</strong></label>
-                  <input type ="text" name= "email" class="form-control" id="inputemail">
-                </div>
-            </div>
-
-         <div class="col-6">
-                <div class="form-group text-center">
-                  <label for="inputname"><strong>Votre numéro de téléphone</strong></label>
-                  <input type ="text" phone= "email" class="form-control" id="inputphone_number">
-                </div>
-            </div>
-    </div>
-
-
-     <div class="row">
-         <div class= "col-12 ">
-                <div class="form-group text-center">
-                <label type="inputmessage"><strong>votre message</strong></label>
-                <textarea id="inputmessage"name=" message" class="form-control"></textarea>
-                </div>
-        </div>
-    
-    <div class="container text-center">
-    <div class="row ">
-        <div class="col 12 ">
-         <button type="submit" class="btn btn-secondary" style="background-color:#3dc0ad">Envoyer</button>
-        </div>
-    </div>
-    </form><br><br>
-</div>
-</div>
-</div>
 </body>
  <footer>
  </footer>
